e2ff5f3720a27ab26f91c7709376edde15266e81
1337Misom <103592904+1337Misom@users.noreply.github.com>
2022-09-17 23:00:16 +0200

    Create build.sh

---
+++ PanLivePlayerContorl/build.sh
@@ -0,0 +1,12 @@
+GREEN='\033[0;32m'
+LIGHTGREEN='\033[1;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "${LIGHTGREEN}Building${NC} PanLivePlayerSDL ${BLUE}(g++ PanLivePlayerControl.c -lSDL2 -lSDL2_image -lcurl -pthread -lturbojpeg -lao -Iincludes/ -o PanLivePlayerControl)${NC}"
+if g++ PanLivePlayerControl.c -lSDL2 -lSDL2_image -lcurl -pthread -lturbojpeg -lao -Iincludes/ -o PanLivePlayerControl ; then
+  echo "${GREEN}Built PanLivePlayerControl Successfully${NC}"
+else
+  echo "${RED}Failed To Build ${GREEN}PanLivePlayerControl ${BLUE}Tipp: Check that you have c++/g++ SDL2 SDL2_image libcurl and libturbojpeg installed.${NC}"
+fi
